1f7c4f0d0798d88df1597eaa9f898b645c9b5587
mikewrosey <mrose@nanthealth.com>
2022-05-06 15:39:37 -0400

    fix login test TS issues

--- ui/tests/login.test.ts
+++ ui/tests/login.test.ts
@@ -3,6 +3,14 @@ import { createTestingPinia } from '@pinia/testing'
 import { test, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 
+interface LoginVm {
+  usernameError: string
+  passwordError: string
+  username: string
+  password: string
+  onLoginBtnClick: () => void
+}
+
 const wrapper = mount(Login, { global: { plugins: [createTestingPinia()] }, props: {} })
 
 test('the login page renders', () => {
@@ -10,9 +18,9 @@ test('the login page renders', () => {
 })
 
 test('login page input errors get set', async () => {
-  wrapper.vm.onLoginBtnClick()
-  expect(wrapper.vm.usernameError).toBe('Username is required.')
-  expect(wrapper.vm.passwordError).toBe('Password is required.')
+  (wrapper.vm as unknown as LoginVm).onLoginBtnClick()
+  expect((wrapper.vm as unknown as LoginVm).usernameError).toBe('Username is required.')
+  expect((wrapper.vm as unknown as LoginVm).passwordError).toBe('Password is required.')
 })
 
 test('username and password refs update with the correct value', async () => {
@@ -20,6 +28,6 @@ test('username and password refs update with the correct value', async () => {
   const passwordInput = wrapper.get('[data-test="password-input"] .feather-input')
   await usernameInput.setValue('admin')
   await passwordInput.setValue('adminpass')
-  expect(wrapper.vm.username).toBe('admin')
-  expect(wrapper.vm.password).toBe('adminpass')
+  expect((wrapper.vm as unknown as LoginVm).username).toBe('admin')
+  expect((wrapper.vm as unknown as LoginVm).password).toBe('adminpass')
 })
