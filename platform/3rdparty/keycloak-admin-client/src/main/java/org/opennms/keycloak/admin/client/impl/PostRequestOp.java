059b07dc42b3c448cb5c53ea92bc182aa1dc4901
Gerald Humphries <geraldhumphries@gmail.com>
2022-05-24 09:33:40 -0400

    HS-43: Remove custom keycloak-admin-client module

--- platform/3rdparty/keycloak-admin-client/src/main/java/org/opennms/keycloak/admin/client/impl/PostRequestOp.java
+++
@@ -1,8 +0,0 @@
-package org.opennms.keycloak.admin.client.impl;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpUriRequest;
-
-public interface PostRequestOp {
-    boolean process(HttpUriRequest request, HttpResponse response);
-}
