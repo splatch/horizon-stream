8681113bd4a36888e7d6dc6b4fa6867dac9ddf24
chinh le <cle@opennms.com>
2022-07-07 16:00:55 -0400

    HS-207 Add deviceTable unit tests

--- ui/src/components/Appliances/deviceTable.spec.ts
+++ ui/src/components/Appliances/deviceTable.spec.ts
@@ -1,8 +1,6 @@
 import { mount } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
-import { useDeviceStore } from '@/store/deviceStore'
 import DeviceTable from './DeviceTable.vue'
-// import flushPromises from 'flush-promises'
 
 describe('DeviceTable.vue', () => {
   describe('Required columns', () => {
@@ -20,48 +18,40 @@ describe('DeviceTable.vue', () => {
       const elem = wrapper.find(`[data-test="${dataTest}"]`)
       expect(elem.exists()).toBe(true)
     })
+  })
     
-    it('should have an empty table when there\'s no device', async () =>{
-      const wrapper = mount(DeviceTable, { 
-        global: { plugins: [createTestingPinia({
-          initialState: { 
-            deviceItems: []
-          }
-        })] }
-      })
-      // flushPromises()
-      const store = useDeviceStore()
-      // console.log('store',store)
-      // store.deviceItems = []
-      // store.$patch({ deviceItems: [] })
-      // expect(store.deviceItems).toEqual([])
-      // console.log('store.deviceItems',store.deviceItems)
-      const deviceItem = wrapper.find('[data-test="device-item"]')
-      // await deviceItem.nextTick()
-      // console.log(deviceItem)
-      expect(deviceItem.exists()).toBe(false)
+  it('should have an empty table when there\'s no device', async () =>{
+    const wrapper = mount(DeviceTable, { 
+      global: { plugins: [createTestingPinia({
+        initialState: { 
+          deviceItems: []
+        }
+      })] }
     })
+      
+    const deviceItem = wrapper.find('[data-test="device-item"]')
+    expect(deviceItem.exists()).toBe(false)
+  })
     
-    it('should display a list when there\'s device', async () => {
-      const deviceItems = [{
-        id: '1',
-        name: 'device1',
-        icmp_latency: 'latency1',
-        snmp_uptime: 'uptime1'
-      }]
+  it('should display a list when there\'s device', async () => {
+    const deviceItems = [{
+      id: '1',
+      name: 'device1',
+      icmp_latency: 'latency1',
+      snmp_uptime: 'uptime1'
+    }]
 
-      const wrapper = mount(DeviceTable, { 
-        global: { plugins: [createTestingPinia({
-          initialState: { 
-            deviceStore: { 
-              deviceItems
-            }
+    const wrapper = mount(DeviceTable, { 
+      global: { plugins: [createTestingPinia({
+        initialState: { 
+          deviceStore: { 
+            deviceItems
           }
-        })] }
-      })
-
-      const deviceItem = wrapper.find('[data-test="device-item"]')
-      expect(deviceItem.exists()).toBe(true)
+        }
+      })] }
     })
+
+    const deviceItem = wrapper.find('[data-test="device-item"]')
+    expect(deviceItem.exists()).toBe(true)
   })
 })
