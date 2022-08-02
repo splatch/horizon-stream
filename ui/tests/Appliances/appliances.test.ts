ad02f98bf5f57ce20c4c9af6c9ae462811a6e6a2
mike <mikewrosey@gmail.com>
2022-08-02 12:34:33 -0400

    stub teleport, fix warnings for container tests

--- ui/tests/Appliances/appliances.test.ts
+++ ui/tests/Appliances/appliances.test.ts
@@ -1,43 +1,41 @@
-import { shallowMount, mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Appliances from '@/containers/Appliances.vue'
 import NotificationsCtrl from '@/components/Appliances/NotificationsCtrl.vue'
 import DeviceTable from '@/components/Appliances/DeviceTable.vue'
 import MinionsTable from '@/components/Appliances/MinionsTable.vue'
-import { createPinia, setActivePinia } from 'pinia'
+import { createPinia } from 'pinia'
 import { useAppliancesStore } from '@/store/Views/appliancesStore'
-import { createClient, setActiveClient } from 'villus'
+import { createClient, VILLUS_CLIENT } from 'villus'
+
+const wrapper = mount(Appliances, { 
+  global: { 
+    plugins: [ createPinia() ],
+    provide: {
+      [VILLUS_CLIENT as unknown as string]: createClient({
+        url: 'http://test/graphql'
+      })
+    }
+  }
+})
 
 describe('Appliances.vue', () => {
-  beforeEach(() => {
-    setActivePinia(createPinia())
-
-    setActiveClient(createClient({
-      url: 'http://test/graphql'
-    }))
-  })
-
   it('should have NotificationsCtrl component', () => {
-    const wrapper = shallowMount(Appliances)
     const notificationsCtrl = wrapper.findComponent(NotificationsCtrl)
-
     expect(notificationsCtrl.exists()).toBe(true)
   })
-  it('should have DeviceTable component', () => {
-    const wrapper = shallowMount(Appliances)
-    const deviceTable = wrapper.findComponent(DeviceTable)
 
+  it('should have DeviceTable component', () => {
+    const deviceTable = wrapper.findComponent(DeviceTable)
     expect(deviceTable.exists()).toBe(true)
   })
-  it('should have MinionsTable component', () => {
-    const wrapper = shallowMount(Appliances)
-    const minionsTable = wrapper.findComponent(MinionsTable)
 
+  it('should have MinionsTable component', () => {
+    const minionsTable = wrapper.findComponent(MinionsTable)
     expect(minionsTable.exists()).toBe(true)
   })
 })
 
 test('The appliancesStore pinia functions should be called', async () => {
-  const wrapper = mount(Appliances)
   const store = useAppliancesStore()
 
   const hideMinionsBtn = wrapper.get('[data-test="hide-minions-btn"]')
