d3c9ef5eedc33d3eb654d191e97ca941281c3881
Łukasz Dywicki <luke@code-house.org>
2022-08-18 22:37:28 +0200

    Relocate ignite-worker to taskste/taskset-worker.
    Corresponding renames included.
    
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

--- minion/ignite-worker/src/main/java/org/opennms/horizon/minion/ignite/worker/ignite/registries/ListenerFactoryRegistry.java
+++
@@ -1,8 +0,0 @@
-package org.opennms.horizon.minion.ignite.worker.ignite.registries;
-
-import org.opennms.horizon.minion.plugin.api.ListenerFactory;
-
-public interface ListenerFactoryRegistry {
-
-    ListenerFactory getService(String type);
-}
--- minion/ignite-worker/src/main/java/org/opennms/horizon/minion/ignite/worker/workflows/WorkflowExecutorLocalService.java
+++
@@ -1,6 +0,0 @@
-package org.opennms.horizon.minion.ignite.worker.workflows;
-
-public interface WorkflowExecutorLocalService {
-    void start() throws Exception;
-    void cancel();
-}
