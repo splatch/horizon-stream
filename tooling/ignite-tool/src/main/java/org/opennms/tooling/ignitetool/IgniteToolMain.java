2f5c9b693807dc00d2130726051be8b2f00b6d6b
Arthur Naseef <art@artnaseef.com>
2022-08-17 22:43:52 -0700

    Added opennms-ignite-tool to query/interact with an Ignite cluster.

---
+++ tooling/ignite-tool/src/main/java/org/opennms/tooling/ignitetool/IgniteToolMain.java
@@ -0,0 +1,12 @@
+package org.opennms.tooling.ignitetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteToolMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgniteToolMain.class, args);
+    }
+}
