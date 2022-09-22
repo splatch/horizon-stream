99f60d98ef5515678634d869d0753d85a4627fdf
Gerald Humphries <geraldhumphries@gmail.com>
2022-09-22 16:44:33 -0400

    Remove local-sample folder
    Fix HS-403

--- local-sample/hs-grafana.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ../grafana/
-
-docker build -t opennms/horizon-stream-grafana:local -f ./Dockerfile . 2> ../local-sample/tmp/HS_GRAFANA.log
-
-echo 1 > ../local-sample/tmp/HS_GRAFANA
