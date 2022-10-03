6784d8074e6849c12e34e0f235a360da2c9cbb1a
Jeffrey-David Kapp <jkapp@opennms.com>
2022-10-03 14:14:35 -0400

    HS-274: couple quick bug fixes

--- operator/internal/util/maps/convert_map_test.go
+++ operator/internal/util/maps/convert_map_test.go
@@ -1,20 +1,23 @@
+//go:build unit
+// +build unit
+
 package maps
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
+    "github.com/stretchr/testify/assert"
+    "testing"
 )
 
 func TestConvertByteToStringMap(t *testing.T) {
-	input := map[string][]byte {
-		"foo": []byte("bar"),
-		"test": []byte("tesssst"),
-	}
-	expected := map[string]string {
-		"foo": "bar",
-		"test": "tesssst",
-	}
+    input := map[string][]byte{
+        "foo":  []byte("bar"),
+        "test": []byte("tesssst"),
+    }
+    expected := map[string]string{
+        "foo":  "bar",
+        "test": "tesssst",
+    }
 
-	res := ConvertByteToStringMap(input)
-	assert.Equal(t, expected, res, "should convert the map correctly")
+    res := ConvertByteToStringMap(input)
+    assert.Equal(t, expected, res, "should convert the map correctly")
 }
