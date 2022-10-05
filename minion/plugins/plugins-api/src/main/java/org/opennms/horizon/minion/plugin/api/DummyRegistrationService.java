a2b4a317d5287250d0b04e40bd5baa4d110c6069
mfrazier <mfrazier@savoirtech.com>
2022-10-05 09:06:24 -0700

    Adding comments for clarification

--- minion/plugins/plugins-api/src/main/java/org/opennms/horizon/minion/plugin/api/DummyRegistrationService.java
+++ minion/plugins/plugins-api/src/main/java/org/opennms/horizon/minion/plugin/api/DummyRegistrationService.java
@@ -3,6 +3,9 @@ package org.opennms.horizon.minion.plugin.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//NOTE: this approach of gathering plugin metadata is still awaiting final requirements and design review.
+// For now, this is a stub to satisfy wiring in blueprint
+
 public class DummyRegistrationService implements RegistrationService {
 
     private final Logger logger = LoggerFactory.getLogger(DummyRegistrationService.class);
