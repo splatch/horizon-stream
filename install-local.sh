83039ae88b1c0c0d4c880e5c47a7d6fc94175fa7
jaymberry <jaymberry@gmail.com>
2022-10-21 13:26:28 -0400

    HS-501 - Added opennms/horizon-stream-inventory:local to install-loca… (#395)
    * HS-501 - Added opennms/horizon-stream-inventory:local to install-local.sh
    
    * HS-501 - added inventory to install-local.sh script
    
    Co-authored-by: jaberry <jaberry@opennms.com>

--- install-local.sh
+++ install-local.sh
@@ -3,14 +3,14 @@
 
 cd operator/
 
-bash scripts/create-kind-cluster.sh
-
 # This determines whether or not to import custom images or not.
 HELP='Need to pass "local" parameter to script'
 if [ -z "$1" ]; then
   echo "$HELP"
 else
   if [ $1 == "local" ]; then
+
+    bash scripts/create-kind-cluster.sh
 
     echo
     echo ________________Installing Operator________________
@@ -22,6 +22,8 @@ else
   
   elif [ "$1" == "custom-images" ]; then
 
+    bash scripts/create-kind-cluster.sh
+
     # Will add a kind-registry here at some point, see .github/ for sample script.
     kind load docker-image opennms/operator:local-build&
     kind load docker-image opennms/horizon-stream-core:local&
@@ -32,6 +34,7 @@ else
     kind load docker-image opennms/horizon-stream-ui:local&
     kind load docker-image opennms/horizon-stream-notification:local&
     kind load docker-image opennms/horizon-stream-rest-server:local&
+    kind load docker-image opennms/horizon-stream-inventory:local&
 
     # Need to wait for the images to be loaded.
     sleep 120
@@ -48,6 +51,16 @@ else
     kubectl apply -f ../install-local-onms-instance.yaml
     if [ $? -ne 0 ]; then exit; fi
 
+  elif [ $1 == "existing-k8s" ]; then
+
+    echo
+    echo ________________Installing Operator________________
+    echo
+    helm upgrade -i operator-local ../charts/opennms-operator -f ../install-local-operator-values.yaml --namespace opennms --create-namespace
+    if [ $? -ne 0 ]; then exit; fi
+
+    bash scripts/create-instance.sh
+  
   else
     echo "$HELP"
   fi
