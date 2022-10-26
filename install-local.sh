1d8a24ad69e8349069dbab1908b67592d513c1d0
jaymberry <jaymberry@gmail.com>
2022-10-26 15:20:06 -0400

    HS-512 - added custom dns options to install-local.sh (#405)
    * HS-512 - added custom dns options to install-local.sh
    
    * HS-512 - added custom domain parameter to .github actions external-it action
    
    * HS-512 - fixed custom images CRD
    
    Co-authored-by: jaberry <jaberry@opennms.com>

--- install-local.sh
+++ install-local.sh
@@ -13,85 +13,101 @@ operator_run () {
   done
 }
 
+# For local, we can setup localhost as the default on port 8080 or something.
+# Like Skaffold and Tilt. 
 # This determines whether or not to import custom images or not.
 HELP='Need to pass "local" parameter to script'
-if [ -z "$1" ]; then
+if [[ -z "$2" || -z "$1" ]]; then
+  echo "Need to add custom DNS for the second parameter, domain to use."
   echo "$HELP"
+  exit 1
 else
-  if [ $1 == "local" ]; then
+  mkdir -p tmp
+  cat install-local-onms-instance.yaml| sed "s/onmshs/$2/g" > tmp/install-local-onms-instance.yaml
+  cat install-local-onms-instance-custom-images.yaml| sed "s/onmshs/$2/g" > tmp/install-local-onms-instance-custom-images.yaml
+fi
 
-    cd operator/
-    bash scripts/create-kind-cluster.sh
+if [ $1 == "local" ]; then
 
-    echo
-    echo ________________Installing Operator________________
-    echo
-    helm upgrade -i operator-local ../charts/opennms-operator -f ../install-local-operator-values.yaml --namespace opennms --create-namespace
-    if [ $? -ne 0 ]; then exit; fi
+  cd operator/
+  bash scripts/create-kind-cluster.sh
 
-    bash scripts/create-instance.sh
- 
-    operator_run
+  echo
+  echo ________________Installing Operator________________
+  echo
+  helm upgrade -i operator-local ../charts/opennms-operator -f ../install-local-operator-values.yaml --namespace opennms --create-namespace
+  if [ $? -ne 0 ]; then exit; fi
 
-  elif [ "$1" == "custom-images" ]; then
+  echo
+  echo ____________Installing Local Instance______________
+  echo
+  kubectl apply -f ../tmp/install-local-onms-instance.yaml
+  if [ $? -ne 0 ]; then exit; fi
 
-    cd operator/
-    bash scripts/create-kind-cluster.sh
+  operator_run
 
-    # Will add a kind-registry here at some point, see .github/ for sample script.
-    kind load docker-image opennms/operator:local-build&
-    kind load docker-image opennms/horizon-stream-core:local&
-    kind load docker-image opennms/horizon-stream-minion:local&
-    kind load docker-image opennms/horizon-stream-minion-gateway:local&
-    kind load docker-image opennms/horizon-stream-keycloak:local&
-    kind load docker-image opennms/horizon-stream-grafana:local&
-    kind load docker-image opennms/horizon-stream-ui:local&
-    kind load docker-image opennms/horizon-stream-notification:local&
-    kind load docker-image opennms/horizon-stream-rest-server:local&
-    kind load docker-image opennms/horizon-stream-inventory:local&
-    kind load docker-image opennms/horizon-stream-metrics-processor:local&
+elif [ "$1" == "custom-images" ]; then
 
-    # Need to wait for the images to be loaded.
-    sleep 120
+  cd operator/
+  bash scripts/create-kind-cluster.sh
 
-    echo
-    echo ________________Installing Operator________________
-    echo
-    helm upgrade -i operator-local ../charts/opennms-operator -f scripts/local-operator-values.yaml --namespace opennms --create-namespace
-    if [ $? -ne 0 ]; then exit; fi
+  # Will add a kind-registry here at some point, see .github/ for sample script.
+  kind load docker-image opennms/operator:local-build&
+  kind load docker-image opennms/horizon-stream-core:local&
+  kind load docker-image opennms/horizon-stream-minion:local&
+  kind load docker-image opennms/horizon-stream-minion-gateway:local&
+  kind load docker-image opennms/horizon-stream-keycloak:local&
+  kind load docker-image opennms/horizon-stream-grafana:local&
+  kind load docker-image opennms/horizon-stream-ui:local&
+  kind load docker-image opennms/horizon-stream-notification:local&
+  kind load docker-image opennms/horizon-stream-rest-server:local&
+  kind load docker-image opennms/horizon-stream-inventory:local&
+  kind load docker-image opennms/horizon-stream-metrics-processor:local&
 
-    echo
-    echo ____________Installing Local Instance______________
-    echo
-    kubectl apply -f ../install-local-onms-instance.yaml
-    if [ $? -ne 0 ]; then exit; fi
- 
-    operator_run
+  # Need to wait for the images to be loaded.
+  sleep 120
 
-  elif [ $1 == "existing-k8s" ]; then
+  echo
+  echo ________________Installing Operator________________
+  echo
+  helm upgrade -i operator-local ../charts/opennms-operator -f scripts/local-operator-values.yaml --namespace opennms --create-namespace
+  if [ $? -ne 0 ]; then exit; fi
 
-    cd operator/
+  echo
+  echo ____________Installing Local Instance______________
+  echo
+  kubectl apply -f ../tmp/install-local-onms-instance-custom-images.yaml
+  if [ $? -ne 0 ]; then exit; fi
 
-    echo
-    echo ________________Installing Operator________________
-    echo
-    helm upgrade -i operator-local ../charts/opennms-operator -f ../install-local-operator-values.yaml --namespace opennms --create-namespace
-    if [ $? -ne 0 ]; then exit; fi
+  operator_run
 
-    bash scripts/create-instance.sh
- 
-    operator_run
- 
-  elif [ $1 == "existing-k8s-no-op" ]; then
+elif [ $1 == "existing-k8s" ]; then
 
-    echo
-    echo ____________Installing HS Instance______________
-    echo
-    helm upgrade -i horizon-stream charts/opennms -f ./install-local-onms-instance.yaml --namespace hs-instance --create-namespace
- 
-  else
-    echo "$HELP"
-  fi
+  cd operator/
+
+  echo
+  echo ________________Installing Operator________________
+  echo
+  helm upgrade -i operator-local ../charts/opennms-operator -f ../install-local-operator-values.yaml --namespace opennms --create-namespace
+  if [ $? -ne 0 ]; then exit; fi
+
+  echo
+  echo ____________Installing Local Instance______________
+  echo
+  kubectl apply -f ../tmp/install-local-onms-instance.yaml
+  if [ $? -ne 0 ]; then exit; fi
+
+  operator_run
+
+elif [ $1 == "existing-k8s-no-op" ]; then
+
+  echo
+  echo ____________Installing HS Instance______________
+  echo
+  helm upgrade -i horizon-stream charts/opennms -f ./tmp/install-local-onms-instance.yaml --namespace hs-instance --create-namespace
+
+else
+  echo "$HELP"
 fi
 
 bash scripts/add-local-ssl-cert.sh
