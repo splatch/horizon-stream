7bc2c9c4e99208e2d819e8c6934ed917c52362b3
Chinh Le <101056132+chinh-cle@users.noreply.github.com>
2022-11-08 08:54:52 -0500

    HS-538 Add page header

---
+++ ui/src/components/Common/PageHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">{{ props.heading }}</div>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  heading: {
+    type: String,
+    default: 'Page Heading'
+  }
+})
+</script>
+<style lang="scss" scoped>
+@use "@featherds/styles/mixins/typography";
+.header {
+  @include typography.headline2();
+  font-weight: bold;
+  margin: 1.5rem 1rem;
+}
+</style>
---
+++ ui/src/containers/Inventory.vue
@@ -0,0 +1,14 @@
+<template>
+  <PageHeader :heading="heading" />
+  <!-- tabs anchor-->
+  <!-- expandable filter: search, select, sorting -->
+  <!-- card: title, metric pill, list anchor, actions -->
+  <div/>
+</template>
+<script lang="ts" setup>
+import PageHeader from '@/components/Common/PageHeader.vue'
+
+const heading = 'Network Inventory'
+</script>
+<style lang="scss" scoped>
+</style>
