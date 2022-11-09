b04939fb4e0f19b7813406f20e6162e43e7cceb4
Chinh Le <101056132+chinh-cle@users.noreply.github.com>
2022-11-09 15:01:26 -0500

    HS-575 Add anchor list

---
+++ ui/src/components/Inventory/ListAnchor.vue
@@ -0,0 +1,24 @@
+<template>
+  <FeatherList>
+    <FeatherListItem v-for="anchor in anchors" :key="anchor.label" :asLi="true">{{ anchor.label }}: <a :href="anchor.link">{{ anchor.value }}</a></FeatherListItem>
+  </FeatherList>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  anchors: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.feather-list-item {
+  cursor: default;
+  height: auto;
+  &:hover {
+    background: none !important;
+  }
+}
+</style>
--- ui/src/containers/Inventory.vue
+++ ui/src/containers/Inventory.vue
@@ -2,12 +2,36 @@
   <PageHeader :heading="heading" />
   <!-- tabs anchor-->
   <!-- expandable filter: search, select, sorting -->
-  <!-- card: title, metric pill, list anchor, actions -->
+  <!-- card: title, metric chip, list anchor, actions -->
+  <ListAnchor :anchors="anchors" />
   <div/>
 </template>
 
 <script lang="ts" setup>
 const heading = 'Network Inventory'
+
+const anchors = [
+  {
+    label: 'Monitoring Profile',
+    value: 75,
+    link: '#'
+  },
+  {
+    label: 'Monitoring Location',
+    value: 'DefaultMinion',
+    link: '#'
+  },
+  {
+    label: 'IP Interface',
+    value: 25,
+    link: '#'
+  },
+  {
+    label: 'Tag',
+    value: 100,
+    link: '#'
+  }
+]
 </script>
 
 <style lang="scss" scoped>
