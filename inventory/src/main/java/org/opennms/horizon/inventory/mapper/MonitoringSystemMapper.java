342b9374bcd5c1c7dc17e7b7021d32a7378bfba9
Yang Li <yali@opennms.com>
2022-11-10 16:55:34 -0500

    changed the MonitoringSystemDTO last_checked_in field to timestamp

--- inventory/src/main/java/org/opennms/horizon/inventory/mapper/MonitoringSystemMapper.java
+++ inventory/src/main/java/org/opennms/horizon/inventory/mapper/MonitoringSystemMapper.java
@@ -1,5 +1,9 @@
 package org.opennms.horizon.inventory.mapper;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 import org.mapstruct.Mapper;
 import org.opennms.horizon.inventory.dto.MonitoringSystemDTO;
 import org.opennms.horizon.inventory.model.MonitoringSystem;
@@ -8,4 +12,13 @@ import org.opennms.horizon.inventory.model.MonitoringSystem;
 public interface MonitoringSystemMapper {
     MonitoringSystem dtoToModel(MonitoringSystemDTO dto);
     MonitoringSystemDTO modelToDTO(MonitoringSystem model);
+
+    default LocalDateTime timeStampToDateTime(long timeStamp) {
+        Instant instant = Instant.ofEpochMilli(timeStamp);
+        return LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
+    }
+
+    default long dateTimeToTimestamp(LocalDateTime dateTime) {
+        return dateTime.toInstant(ZoneOffset.UTC).toEpochMilli();
+    }
 }
