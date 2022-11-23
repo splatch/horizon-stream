2236169d5e374e2c2313721edf9a76489c362975
Chinh Le <101056132+chinh-cle@users.noreply.github.com>
2022-11-22 19:55:40 -0500

    HS-535 Reduce unneeded TabList component

--- ui/src/containers/Inventory.vue
+++ ui/src/containers/Inventory.vue
@@ -1,24 +1,88 @@
 <template>
-  <PageHeader :heading="heading" class="mx-l" />
-  <!-- tabs anchor-->
-  <!-- expandable filter: search, select, sorting -->
-  <Filter @selected-item="onSelectedItem"/>
-  <!-- card: title, metric chip list, text anchor list, icon action list -->
-
-  <!-- This ctrl should later be moved into Filter. -->
-  <TagManagerCtrl />
-  <TagManager />
+  <PageHeader :heading="'Network Inventory'" class="mx-l" />
+  <FeatherTabContainer class="tab-container">
+    <template v-slot:tabs>
+      <FeatherTab v-for="tab in tabs" :key="tab.label">{{ tab.label }}</FeatherTab>
+    </template>
+    <FeatherTabPanel v-for="tab in tabs" :key="tab.label">
+      <Filter />
+      <DetectedNodesTabContent v-if="tab.type === NodeDetailContentType.DETECTED" :tab="tab" />
+      <MonitoredNodesTabContent v-else :tab="tab" />
+    </FeatherTabPanel>
+  </FeatherTabContainer>
 </template>
 
 <script lang="ts" setup>
-import { ISelectItemType } from '@featherds/select/src/components/types'
+import {
+  FeatherTab,
+  FeatherTabContainer,
+  FeatherTabPanel
+} from '@featherds/tabs'
+import Filter from '@/components/Inventory/Filter.vue'
+import DetectedNodesTabContent from '@/components/Inventory/DetectedNodesTabContent.vue'
+import MonitoredNodesTabContent from '@/components/Inventory/MonitoredNodesTabContent.vue'
+import { NodeDetailContentType } from '@/types'
+import { TabNode } from '@/types/inventory'
 
-const heading = 'Network Inventory'
-
-const onSelectedItem = (selectedItem: ISelectItemType) => {
-  console.log('onSelectedItem',selectedItem)
-}
+const tabs: TabNode[] = [
+  {
+    type: NodeDetailContentType.MONITORED,
+    label: 'Monitored Nodes',
+    nodes: [
+      {
+        header: 'Node 1'
+      },
+      {
+        header: 'Node 11'
+      }
+    ]
+  },
+  {
+    type: NodeDetailContentType.MONITORED,
+    label: 'Unmonitored Nodes',
+    nodes: [
+      {
+        header: 'Node 2'
+      },
+      {
+        header: 'Node 22'
+      }
+    ]
+  },
+  {
+    type: NodeDetailContentType.DETECTED,
+    label: 'Detected Nodes',
+    nodes: [
+      {
+        header: 'Node 3'
+      },
+      {
+        header: 'Node 33'
+      }
+    ]
+  }
+]
 </script>
 
 <style lang="scss" scoped>
+@use '@featherds/styles/themes/variables';
+
+.tab-container {
+  margin: 0 var(variables.$spacing-l);
+  :deep(> ul) {
+    display: flex;
+    border-bottom: 1px solid var(variables.$disabled-text-on-surface);
+    > li {
+      display: flex !important;
+      flex-grow: 1;
+      > button {
+        display: flex;
+        flex-grow: 1;
+        > span {
+          flex-grow: 1;
+        }
+      }
+    }
+  }
+}
 </style>
