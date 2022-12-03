0a61635a12ad36ea86ac77769edf7c9fa6f05314
chinh le <cle@opennms.com>
2022-12-03 13:46:29 -0500

    HS-624 Fix  Sonar XSS error

--- ui/src/components/Inventory/TextAnchorList.vue
+++ ui/src/components/Inventory/TextAnchorList.vue
@@ -2,58 +2,40 @@
   <ul class="text-anchor-list">
     <li data-test="profile">
       <label :for="label.profile">{{ label.profile }}: </label>
-      <a v-if="anchorProps.profileLink" @click="router.push({path: anchorProps.profileLink})" :id="label.profile">{{ anchor?.profileValue }}</a>
-      <!-- <a v-if="anchorProps.profileLink" :href="anchorProps.profileLink" :id="label.profile">{{ anchor?.profileValue }}</a> -->
-      <!-- <a v-if="anchor.profileLink" :href="anchorProps.profileLink" :id="label.profile">{{ anchor?.profileValue }}</a> -->
-      <span v-else :id="label.profile">{{ anchor?.profileValue }}</span>
+      <a v-if="anchor.profileLink" @click="goto(anchor.profileLink || '')" :id="label.profile">{{ anchor?.profileValue }}</a>
+      <span v-else :id="label.profile">{{ anchor.profileValue }}</span>
     </li>
     <li data-test="location">
       <label :for="label.location">{{ label.location }}: </label>
-      <a v-if="anchor?.locationLink" :href="anchor?.locationLink" :id="label.location">{{ anchor?.locationValue }}</a>
-      <span v-else :id="label.location">{{ anchor?.locationValue }}</span>
+      <a v-if="anchor.locationLink" @click="goto(anchor.locationLink || '')" :id="label.location">{{ anchor?.locationValue }}</a>
+      <span v-else :id="label.location">{{ anchor.locationValue }}</span>
     </li>
     <li data-test="management-ip">
       <label :for="label.managementIp">{{ label.managementIp }}: </label>
-      <a v-if="anchor?.managementIpLink" :href="anchor?.managementIpLink" :id="label.managementIp">{{ anchor?.managementIpValue }}</a>
-      <span v-else :id="(label.managementIp)">{{ anchor?.managementIpValue }}</span>
+      <a v-if="anchor.managementIpLink" @click="goto(anchor.managementIpLink || '')" :id="label.managementIp">{{ anchor.managementIpValue }}</a>
+      <span v-else :id="(label.managementIp)">{{ anchor.managementIpValue }}</span>
     </li>
     <li data-test="tag">
       <label :for="label.tag">{{ label.tag }}: </label>
-      <a v-if="anchor?.tagLink" :href="anchor?.tagLink" :id="label.tag">{{ anchor?.tagValue }}</a>
-      <span v-else :id="label.tag">{{ anchor?.tagValue }}</span>
+      <a v-if="anchor.tagLink" @click="goto(anchor.tagLink || '')" :id="label.tag">{{ anchor.tagValue }}</a>
+      <span v-else :id="label.tag">{{ anchor.tagValue }}</span>
     </li>
   </ul>
 </template>
 
 <script lang="ts" setup>
-import { PropType } from 'vue'
 import { Anchor } from '@/types/inventory'
-// import { Router } from 'vue-router'
 import router from '@/router'
 
-const props = defineProps<{
+defineProps<{
   anchor: Anchor
 }>()
-/* const props = defineProps({
-  anchor: {
-    type: Object as PropType<Anchor>,
-    required: true
-  }
-}) */
 
-const anchorProps = computed(() => ({
-  ...props,
-  // profileLink: props.profileLink
-  profileLink: '/'
-}))
-
-const xss = () => {
-  console.log('XSS!!!')
-  // alert('XSS!!!')
-  // router.replace({ path: '/' })
-  router.push({ path: '/' })
+const goto = (path: string) => {
+  router.push({
+    path
+  })
 }
-console.log(router)
 
 const label = {
   profile: 'Monitoring Profile',
