f2bf8e3a0569a8d85553ff141021e13662ce90de
Anya <anya.rybalova@gmail.com>
2023-01-06 15:10:13 -0500

    HS-762 - add 2 discovery cards

---
+++ ui/src/components/Discovery/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+    <div class="title">{{ title }}</div>
+    <div class="content">
+      <img
+        class="img"
+        :src="imgUrl"
+      />
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+	title: string
+	imgUrl?: string
+}>()
+</script>
+
+<style scoped lang="scss">
+@use '@featherds/styles/themes/variables';
+
+.card {
+	width: 37%;
+	border: 1px solid #d8d8d8;
+	border-radius: 5px;
+	background-color: #ffffff;
+	margin-right: 20px;
+}
+
+.title {
+	padding: var(variables.$spacing-l) var(variables.$spacing-m);
+	background-color: #e8e8e8;
+	font-size: 18px;
+	font-weight: 600;
+}
+
+.content {
+	.img {
+		width: 100%;
+    min-height: 170px;
+	}
+}
+</style>
