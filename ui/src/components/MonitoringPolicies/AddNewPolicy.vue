a8807d534fc2ad68bad31e8186a9164b1bf22e4c
Anya <anya.rybalova@gmail.com>
2023-01-16 15:57:20 -0500

    HS-819 - add new policy

--- ui/src/components/MonitoringPolicies/AddNewPolicy.vue
+++ ui/src/components/MonitoringPolicies/AddNewPolicy.vue
@@ -12,15 +12,23 @@
         secondary
         @click="showTags = !showTags"
       >
-        Add Tags <FeatherIcon :icon="showTags ? ExpandLess : ExpandMore" />
+        Add Tags
+        <FeatherIcon :icon="showTags ? Icons.ExpandLess : Icons.ExpandMore" />
       </FeatherButton>
+      <div class="list-selected-tags">
+        <div class="selected-tag" v-for="tag in store.policy.tags" :key="tag">
+          {{ tag
+          }}<FeatherIcon
+            class="remove-icon"
+            :icon="Icons.Cancel"
+            @click="store.removeTag(tag)"
+          />
+        </div>
+      </div>
     </div>
   </div>
   <div class="tags-section">
-    <PolicyTags
-      v-if="showTags"
-      :tags="tags"
-    />
+    <div v-if="showTags">Tag manager component</div>
   </div>
 </template>
 
@@ -29,44 +37,74 @@ import { useMonitoringPoliciesStore } from '@/store/Views/monitoringPoliciesStor
 const store = useMonitoringPoliciesStore()
 import ExpandMore from '@featherds/icon/navigation/ExpandMore'
 import ExpandLess from '@featherds/icon/navigation/ExpandLess'
-const showTags = ref(false)
+import Cancel from '@featherds/icon/navigation/Cancel'
 
-const tags = ref(['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7', 'tag8', 'tag9', 'tag6=10', 'tag11', 'tag12', 'tag13', 'tag14', 'tag15', 'tag16', 'tag17', 'tag18', 'tag19'])
+const Icons = markRaw({
+  ExpandMore,
+  ExpandLess,
+  Cancel
+})
+const showTags = ref(false)
 </script>
 
 <style scoped lang="scss">
 @use '@featherds/styles/themes/variables';
-@use "@featherds/styles/mixins/typography";
+@use '@featherds/styles/mixins/typography';
 
 .new-policy {
-    background-color: var(variables.$surface);
-  	padding: var(variables.$spacing-l);
-  	margin-top: var(variables.$spacing-m);
-    border: 1px var(variables.$shade-4) solid;
-    width: 80%;
+  background-color: var(variables.$surface);
+  padding: var(variables.$spacing-l);
+  margin-top: var(variables.$spacing-m);
+  border: 1px var(variables.$shade-4) solid;
+  width: 80%;
 
-    .title {
-       @include typography.headline4;
-       margin-bottom: var(variables.$spacing-s);
+  .title {
+    @include typography.headline4;
+    margin-bottom: var(variables.$spacing-s);
+  }
+
+  .name-policy {
+    width: 350px;
+    margin-right: var(variables.$spacing-l);
+  }
+  .form-new-policy {
+    display: flex;
+    .btn-add-tag {
+      height: 40px;
+    }
+  }
+  .list-selected-tags {
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: var(variables.$spacing-l);
+    width: 70%;
+
+    > .selected-tag {
+      background-color: var(variables.$shade-1);
+      padding-left: var(variables.$spacing-m);
+      padding-right: var(variables.$spacing-s);
+      margin-right: var(variables.$spacing-s);
+      border-radius: 5px;
+      height: 26px;
+      margin-bottom: var(variables.$spacing-s);
+      display: flex;
+      align-items: center;
+      color: var(variables.$primary-text-on-color);
     }
 
-    .name-policy {
-        width: 350px;
-        margin-right: var(variables.$spacing-l);
+    .remove-icon {
+      font-size: 18px;
+      cursor: pointer;
+      margin-left: var(variables.$spacing-xs);
     }
-    .form-new-policy {
-        display: flex;
-        .btn-add-tag {
-            height: 40px;
-        }
-    }
+  }
 }
 
 .tags-section {
-    width: 80%;
+  width: 80%;
 }
 
 :deep(.feather-input-sub-text) {
-    display: none;
+  display: none;
 }
 </style>
--- ui/src/store/Views/monitoringPoliciesStore.ts
+++ ui/src/store/Views/monitoringPoliciesStore.ts
@@ -1,12 +1,30 @@
 import { defineStore } from 'pinia'
+import { without } from 'lodash'
 
-export const useMonitoringPoliciesStore = defineStore('monitoringPoliciesStore', {
-  state: () => ({
-    policy: {
-      id: '',
-      name: '',
-      tags: '',
-      rules: ''
+type TState = {
+  policy: {
+    id: string
+    name: string
+    tags: string[]
+    rules: string[]
+  }
+}
+
+export const useMonitoringPoliciesStore = defineStore(
+  'monitoringPoliciesStore',
+  {
+    state: (): TState => ({
+      policy: {
+        id: '',
+        name: '',
+        tags: [],
+        rules: []
+      }
+    }),
+    actions: {
+      removeTag(tag: string) {
+        this.policy.tags = without(this.policy.tags, tag)
+      }
     }
-  })
-})
+  }
+)
