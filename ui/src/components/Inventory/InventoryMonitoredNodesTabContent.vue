5e02c0bb510381cbeb6f2d79cd4456373cb3251c
chinh le <cle@opennms.com>
2023-01-17 07:23:38 -0500

    HS-781 Add tagging overlay element

--- ui/src/components/Inventory/InventoryMonitoredNodesTabContent.vue
+++ ui/src/components/Inventory/InventoryMonitoredNodesTabContent.vue
@@ -1,26 +1,7 @@
-<template>
-  <ul>
-    <li v-for="node in tabContent" :key="node?.id">
-      <section class="header">
-        <Icon :icon="icon" data-test="icon" />
-        <h4 data-test="heading">{{ node?.label }}</h4>
-      </section>
-      <section class="node-content">
-        <div>
-          <FeatherChipList label="List of metric chips" data-test="metric-chip-list">
-            <MetricChip v-for="metric in node?.metrics" :key="metric?.type" :metric="metric" />
-          </FeatherChipList>
-          <InventoryTextAnchorList :anchor="node?.anchor" data-test="text-anchor-list" />
-        </div>
-        <InventoryIconActionList :node="node" class="icon-action" data-test="icon-action-list" />
-      </section>
-    </li>
-  </ul>
-</template>
-
 <script lang="ts" setup>
 import { PropType } from 'vue'
 import Storage from '@material-design-icons/svg/outlined/storage.svg'
+import Checkbox from '@material-design-icons/svg/outlined/check_box.svg'
 import { NodeContent } from '@/types/inventory'
 import { IIcon } from '@/types'
   
@@ -31,20 +12,136 @@ defineProps({
   }
 })
 
-const icon: IIcon = {
+const tagsSelected = ['tag1tag1 tag1tag1tag1tag1tag1tag1', 'tag2', 'tag3']
+const tagChecked = ref(false)
+
+const storage: IIcon = {
   image: Storage,
   title: 'Node'
 }
+const checkbox: IIcon = {
+  image: Checkbox,
+  title: ''
+}
 </script>
+
+<template>
+  <ul class="cards">
+    <li v-for="node in tabContent" :key="node?.id">
+      <section class="header">
+        <Icon :icon="storage" data-test="icon-storage" />
+        <h4 data-test="heading">{{ node?.label }}</h4>
+      </section>
+      <section class="content">
+        <div>
+          <FeatherChipList label="List of metric chips" data-test="metric-chip-list">
+            <MetricChip v-for="metric in node?.metrics" :key="metric?.type" :metric="metric" />
+          </FeatherChipList>
+          <InventoryTextAnchorList :anchor="node?.anchor" data-test="text-anchor-list" />
+        </div>
+        <InventoryIconActionList :node="node" class="icon-action" data-test="icon-action-list" />
+      </section>
+      <div class="overlay">
+        <feather-checkbox id="tagged" v-model="tagChecked" class="tag-node-checkbox"/>
+        <section class="overlay-header">
+          <Icon :icon="storage" data-test="icon-storage" />
+          <h4 data-test="heading">{{ node?.label }}</h4>
+        </section>
+        <section class="overlay-content">
+          <div class="title"><label for="iconCheckbox">Tagged</label><Icon :icon="checkbox" data-test="icon-checkbox" /></div>
+          <FeatherChipList condensed label="Tag list">
+            <FeatherChip v-for="tag in tagsSelected" :key="tag">{{ tag }}</FeatherChip>
+          </FeatherChipList>
+        </section>
+      </div>
+    </li>
+  </ul>
+</template>
 
 <style lang="scss" scoped>
 @use "@featherds/styles/themes/variables";
+@use "@/styles/vars";
 
-ul {
+.overlay {
+  $color-header-title: white;
+
+  position: absolute;
+  lefT: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, .75);
+  padding: var(variables.$spacing-l) var(variables.$spacing-l);
+  border-radius: 0 vars.$border-radius-m vars.$border-radius-m 0;
+  .tag-node-checkbox {
+    position: absolute;
+    top: 0;
+    right: 0;
+    :deep {
+      label {
+        display: none;
+      }
+      .feather-checkbox {
+        .box {
+          border-color: $color-header-title;
+        }
+        &[aria-checked=true] {
+          .box {
+            border-color: var(variables.$primary);
+          }
+        }
+      }
+    }
+  }
+  > .overlay-header {
+    margin-bottom: var(variables.$spacing-xs);
+    margin-right: var(variables.$spacing-s);
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+    align-items: center;
+    color: $color-header-title;
+    > h4 {
+      color: $color-header-title;
+    }
+  }
+  > .overlay-content {
+    > .title {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      color: $color-header-title;
+    }
+    label {
+      font-size: 1.5rem;
+      font-weight: bold;
+    }
+    :deep(svg) {
+      width: 3em;
+      height: 3em;
+      margin-left: var(variables.$spacing-xs);
+    }
+    .chip-list {
+      margin-top: var(variables.$spacing-s);
+      :deep {
+        .chip {
+          margin: 0;
+          background-color: var(variables.$primary);
+          .label {
+            color: var(variables.$primary-text-on-color);
+          }
+        }
+      }
+    }
+  }
+}
+
+ul.cards {
   display: flex;
   flex-flow: row wrap;
   gap: 1rem;
   > li {
+    position: relative;
     padding: var(variables.$spacing-l) var(variables.$spacing-l);
     border: 1px solid var(variables.$secondary-text-on-surface); 
     border-radius: 10px;
@@ -60,7 +157,7 @@ ul {
       }
     }
   }
-  .node-content {
+  .content {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -70,6 +167,6 @@ ul {
 
 .chip-list {
   margin: 0 0 var(variables.$spacing-s);
-  gap: 1rem;
+  gap: 0.5rem;
 }
 </style>
