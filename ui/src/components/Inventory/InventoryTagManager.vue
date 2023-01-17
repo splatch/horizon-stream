7f2b5f56fa92f4180fe6435d69c11cac92617239
chinh le <cle@opennms.com>
2023-01-17 04:04:26 -0500

    HS-781 Style ui

--- ui/src/components/Inventory/InventoryTagManager.vue
+++ ui/src/components/Inventory/InventoryTagManager.vue
@@ -13,11 +13,8 @@ const newTagDropdown = ref()
 const tagNodes = ref()
 const searchValue = ref()
 const selectedTags = ref<string[]>([])
-const tagSelected = ref<Record<string, boolean>>({})
 
-const tags = computed(() => [
-  'tag1tag1 tag1tag1tag1tag1tag1tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7', 'tag8', 'tag9', 'tag10', 'tag11', 'tag12', 'tag13', 'tag14', 'tag15', 'tag16', 'tag17', 'tag18', 'tag19', 'tag20', 'tag21', 'tag22'
-])
+const tags = computed(() => ['tag1tag1 tag1tag1tag1tag1tag1tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7', 'tag8', 'tag9', 'tag10', 'tag11', 'tag12', 'tag13', 'tag14', 'tag15', 'tag16', 'tag17', 'tag18', 'tag19', 'tag20', 'tag21', 'tag22'])
 
 const selectTag = (tag: string) => {
   if (selectedTags.value.includes(tag)) {
@@ -30,7 +27,7 @@ const selectTag = (tag: string) => {
 const addTag = () => {
   // send newtag.value
   newTag.value = '' // clear input
-  newTagDropdown.value.handleClose() // close dropdown
+  newTagDropdown.value.handleClose() // clost dropdown
 }
 </script>
 
@@ -38,31 +35,48 @@ const addTag = () => {
   <div class="tag-manager-box" v-if="inventoryStore.isTaggingBoxOpen">
     <section class="select-tags">
       <div class="top">
-        <h4>Select Tags:</h4>
-        <FeatherInput
-          class="search"
-          v-model="searchValue"
-          label="Search Tags">
-          <template v-slot:post>
-            <FeatherIcon :icon="searchIcon" />
-          </template>
-        </FeatherInput>
+        <div class="heading-total-selected">
+          <h4>Select Tags:</h4>
+          <div class="total-selected">
+            <div>TOTAL: <span>{{ tags.length }}</span><span class="pipe">|</span></div>
+            <div>SELECTED: <span>{{ selectedTags.length }}</span></div>
+          </div>
+        </div>
+        <div class="search-add">
+          <!-- Add tag -->
+          <FeatherDropdown ref="newTagDropdown" class="add-tag-dropdown">
+            <template v-slot:trigger="{ attrs, on }">
+              <FeatherButton link href="#" icon="Add Tag" v-bind="attrs" v-on="on">
+                <FeatherIcon :icon="addIcon" />
+              </FeatherButton>
+            </template>
+            <FeatherInput v-model="newTag" label="New tag" class="new-tag-input" />
+            <FeatherButton primary class="new-tag-btn" @click="addTag">
+              Add Tag
+            </FeatherButton>
+          </FeatherDropdown>
+          <!-- Search tags input -->
+          <FeatherInput
+            class="search"
+            v-model="searchValue"
+            label="Search Tags">
+            <template v-slot:post>
+              <FeatherIcon :icon="searchIcon" />
+            </template>
+          </FeatherInput>
+        </div>
       </div>
-      <FeatherList class="tag-list">
-        <FeatherListItem v-for="tag in tags" :key="tag" asLi>
-          <FeatherCheckbox :id="tag" v-model="tagSelected[tag]" @update:model-value="selectTag(tag)"
-            >{{ tag }}
-          </FeatherCheckbox>
-        </FeatherListItem>
-      </FeatherList>
-    </section>
-    <section class="selected-tags">
-      <h4>Selected Tags:</h4>
-      <FeatherList class="tag-list">
-        <FeatherListItem v-for="tag in selectedTags" :key="tag" asLi
-          >{{ tag }}
-        </FeatherListItem>
-      </FeatherList>
+      <FeatherChipList condensed label="Tags" :key="selectedTags.toString()">
+        <FeatherChip 
+          v-for="tag of tags" 
+          :key="tag" 
+          class="pointer"
+          :class="{ 'selected' : selectedTags.includes(tag) }"
+          @click="selectTag(tag)"
+        >
+          {{ tag }}
+        </FeatherChip>
+      </FeatherChipList>
     </section>
     <section class="tag-nodes">
       <h4>Tag Nodes:</h4>
@@ -96,6 +110,7 @@ const addTag = () => {
     padding-top: 3px;
   }
 }
+
 .select-tags {
   display: flex;
   flex-direction: column;
@@ -105,99 +120,110 @@ const addTag = () => {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    :deep(.search) {
-      width: 200px;
-    }
-  }
-  :deep(.tag-list) {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    > li {
-      label {
-        width: 100px;
-        overflow: scroll;
-        white-space: nowrap;
-        text-overflow: ellipsis;
+    margin-bottom: var(variables.$spacing-m);
+    .heading-total-selected {
+      .total-selected {
+        display: flex;
+        flex-direction: row;
+        padding-top: 8px;
+        > * {
+          > span {
+            font-weight: bold;
+            &.pipe {
+              color: var(variables.$secondary-text-on-surface);
+              margin: 0 var(variables.$spacing-s);
+            }
+          }
+        }
+      }
+
+      @include mediaQueries.screen-md {
+        display: flex;
+        flex-direction: row;
+        width: 50%;
+        justify-content: space-between;
       }
     }
-  }
+    .search-add {
+      display: flex;
+      flex-direction: row;
+      .add-tag-dropdown {
+        .new-tag-input {
+          margin: 0px 10px 0px 10px;
+          width: 175px;
+        }
+        .new-tag-btn {
+          margin-left: var(variables.$spacing-s);
+        }
+        .feather-menu-dropdown {
+          color: red;
+        }
+      }
 
+      :deep(.search) {
+        width: 200px;
+      }
+    }
+
+    @include mediaQueries.screen-md {
+      margin-bottom: 0;
+    }
+  }
   @include mediaQueries.screen-lg {
-    width: 45%;
+    width: 75%;
     min-width: 0;
   }
   @include mediaQueries.screen-xxl {
-    width: 50%;
+    width: 80%;
   }
 }
-.selected-tags {
-  width: 100%;
-  min-width: 445px;
-  margin-top: var(variables.$spacing-m);
-  padding-top: var(variables.$spacing-m);
-  border-top: 1px solid var(variables.$secondary-text-on-surface);
-  :deep(.tag-list) {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    > li {
-      cursor: default;
-      &:hover {
-        background: none;
-      }
-      > span {
-        width: 80px;
-        overflow: scroll;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-      }
-    }
-  }
 
-  @include mediaQueries.screen-lg {
-    width: 35%;
-    min-width: 0;
-    margin-top: 0;
-    padding-top: 0;
-    padding-left: var(variables.$spacing-m);
-    border-top: 0;
-    border-left: 1px solid var(variables.$secondary-text-on-surface);
-    h4 {
-      margin-bottom: 26px;
-    }
-  }
-  @include mediaQueries.screen-xxl {
-    width: 30%;
-  }
-}
 .tag-nodes {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   width: 100%;;
   min-width: 445px;
   margin-top: var(variables.$spacing-m);
   padding-top: var(variables.$spacing-m);
   border-top: 1px solid var(variables.$secondary-text-on-surface);
+
   :deep(.select-tag-nodes) {
     margin-left: var(variables.$spacing-m);
+    > label {
+      display: none;
+    }
+    .feather-radio-group {
+      display: flex;
+      flex-direction: row;
+      .layout-container {
+        margin-right: var(variables.$spacing-m);
+      }
+    }
     .feather-input-sub-text {
       display: none;
     }
   }
 
   @include mediaQueries.screen-lg {
-    width: 15%;
+    width: 20%;
     min-width: 0;
     margin-top: 0;
     padding-top: 0;
-    padding-left: var(variables.$spacing-m);
     border-top: 0;
     border-left: 1px solid var(variables.$secondary-text-on-surface);
-    h4 {
-      margin-bottom: 20px;
+    padding-left: var(variables.$spacing-l);
+    display: block;
+
+    :deep(.select-tag-nodes) {
+      .feather-radio-group {
+        display: block;
+        > * {
+          margin-bottom: var(variables.$spacing-xs);
+        }
+      }
     }
   }
+
   @include mediaQueries.screen-xxl {
     width: 15%;
   }
