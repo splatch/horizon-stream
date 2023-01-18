a12665fad4ad389e8f9d987328be7f2e8dc6c735
Francesco <f.buccini@keybird.de>
2023-01-18 09:37:18 +0100

    DC-456 Add third Module containing SpringBoot Application

--- metrics-processor/main-metrics/src/main/java/org/opennms/Main.java
+++ metrics-processor/main-metrics/src/main/java/org/opennms/Main.java
@@ -1,6 +1,7 @@
-package org.opennms.flows;
+package org.opennms;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
--- metrics-processor/timeseries/src/main/java/org/opennms/horizon/tsdata/Main.java
+++ metrics-processor/timeseries/src/main/java/org/opennms/horizon/tsdata/Main.java
@@ -1,6 +1,7 @@
-package org.opennms.flows;
+package org.opennms.horizon.tsdata;
 
 public class Main {
     public static void main(String[] args) {
+
     }
 }
