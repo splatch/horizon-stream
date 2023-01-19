6e100077a612745d143ce32d6e05a6829b2b9051
chinh le <cle@opennms.com>
2023-01-19 13:38:51 -0500

    Move template tag to top

--- ui/src/components/Common/Icon.vue
+++ ui/src/components/Common/Icon.vue
@@ -7,6 +7,21 @@ This component is used as a wrapper, over FeatherIcon component, to facilitating
   - @material-design-icons: only width/height available
   - @featherds: only viewBox available
  */
+ <template>
+  <FeatherTooltip
+    :title="icon.tooltip || ''"
+    v-slot="{ attrs, on }"
+  >
+    <FeatherIcon
+      v-bind="icon.tooltip ? attrs : null" 
+      v-on="on" 
+      :icon="icon.image" 
+      :title="icon.title" 
+      :viewBox="setViewBox(icon.image)"
+    />
+  </FeatherTooltip>
+</template>
+
 <script lang="ts" setup>
 import { PropType } from 'vue'
 import { setViewBox } from '@/components/utils'
@@ -21,21 +36,6 @@ const props = defineProps({
 
 const iconSize = props.icon.size || 'inherit'
 </script>
-
-<template>
-  <FeatherTooltip
-    :title="icon.tooltip || ''"
-    v-slot="{ attrs, on }"
-  >
-    <FeatherIcon
-      v-bind="icon.tooltip ? attrs : null" 
-      v-on="on" 
-      :icon="icon.image" 
-      :title="icon.title" 
-      :viewBox="setViewBox(icon.image)"
-    />
-  </FeatherTooltip>
-</template>
 
 <style lang="scss" scoped>
 // FeatherIcon default width/height: 1rem
