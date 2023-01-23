bf9c44e8ae93fc64f0cf27dff99be9fbdb64761a
Yang Li <yali@opennms.com>
2023-01-23 12:30:08 -0500

    Initial node scan plugin with ICMP detector and SNMP detector

---
+++ minion/plugins/nodescan-plugin/src/main/java/org/opennms/horizon/minion/nodescan/NodeScannerManager.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2023 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2023 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.horizon.minion.nodescan;
+
+import org.opennms.horizon.minion.icmp.IcmpDetector;
+import org.opennms.horizon.minion.plugin.api.Scanner;
+import org.opennms.horizon.minion.plugin.api.ScannerManager;
+import org.opennms.horizon.minion.plugin.api.registries.DetectorRegistry;
+import org.opennms.horizon.minion.snmp.SnmpDetector;
+
+public class NodeScannerManager implements ScannerManager {
+    private static final String ICMP_DETECTOR = "ICMPDetector";
+    private static final String SNMP_DETECTOR = "SNMPDetector";
+    private final DetectorRegistry detectorRegistry;
+
+    public NodeScannerManager(DetectorRegistry detectorRegistry) {
+        this.detectorRegistry = detectorRegistry;
+    }
+
+    @Override
+    public Scanner create() {
+        return new NodeScanner((IcmpDetector) detectorRegistry.getService(ICMP_DETECTOR).create(), (SnmpDetector) detectorRegistry.getService(SNMP_DETECTOR).create());
+    }
+}
