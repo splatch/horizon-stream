OpenNMS-Cloud/lokahi

Add generic configuration table and inventory
HS-704

inventory/src/main/java/org/opennms/horizon/inventory/model/Configuration.java
@@ -1,0 +1,69 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2023 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2023 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.horizon.inventory.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+public class Configuration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Column(name = "tenant_id")
+    private String tenantId;
+
+    @NotNull
+    @Column(name = "key")
+    private String key;
+
+    @Column(name = "location")
+    private String location;
+
+    @NotNull
+    @Column(columnDefinition = "jsonb", name = "value")
+    @Type(type = "com.vladmihalcea.hibernate.type.json.JsonBinaryType")
+    private Object value;
+}

inventory/src/test/java/org/opennms/horizon/inventory/grpc/ConfigurationGrpcTest.java
@@ -1,0 +1,93 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.horizon.inventory.grpc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.keycloak.common.VerificationException;
+import org.opennms.horizon.inventory.dto.ConfigurationDTO;
+import org.opennms.horizon.inventory.dto.ConfigurationList;
+import org.opennms.horizon.inventory.dto.ConfigurationServiceGrpc;
+import org.opennms.horizon.inventory.service.ConfigurationService;
+
+import com.google.protobuf.Empty;
+
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.stub.MetadataUtils;
+
+//This is an example of gRPC integration tests underline mock services.
+public class ConfigurationGrpcTest extends AbstractGrpcUnitTest {
+    private ConfigurationServiceGrpc.ConfigurationServiceBlockingStub stub;
+    private ConfigurationService mockLocationService;
+    private ConfigurationDTO configuration1, configuration2;
+
+    @BeforeEach
+    public void prepareTest() throws VerificationException, IOException {
+        mockLocationService = mock(ConfigurationService.class);
+        ConfigurationGrpcService grpcService = new ConfigurationGrpcService(mockLocationService, tenantLookup);
+        startServer(grpcService);
+        stub = ConfigurationServiceGrpc.newBlockingStub(grpCleanup.register(InProcessChannelBuilder.forName(serverName).directExecutor().build()));
+        configuration1 = ConfigurationDTO.newBuilder().setTenantId(tenantId).build();
+        configuration2 = ConfigurationDTO.newBuilder().setTenantId(tenantId).build();
+    }
+
+    @AfterEach
+    public void afterTest() {
+        verifyNoMoreInteractions(mockLocationService);
+        verifyNoMoreInteractions(spyInterceptor);
+        reset(mockLocationService, spyInterceptor);
+    }
+
+
+    @Test
+    void testListConfigurations() throws VerificationException {
+        doReturn(Arrays.asList(configuration1, configuration2)).when(mockLocationService).findByTenantId(anyString());
+        ConfigurationList result = stub.withInterceptors(MetadataUtils.newAttachHeadersInterceptor(createHeaders())).listConfigurations(Empty.newBuilder().build());
+        assertThat(result.getConfigurationList().size()).isEqualTo(2);
+        verify(mockLocationService).findByTenantId(tenantId);
+        verify(spyInterceptor).verifyAccessToken(authHeader);
+        verify(spyInterceptor).interceptCall(any(ServerCall.class), any(Metadata.class), any(ServerCallHandler.class));
+    }
+}

inventory/src/test/java/org/opennms/horizon/inventory/service/ConfigurationServiceTest.java
@@ -1,0 +1,93 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.horizon.inventory.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.mockito.ArgumentCaptor;
+import org.opennms.horizon.inventory.dto.ConfigurationDTO;
+import org.opennms.horizon.inventory.mapper.ConfigurationMapper;
+import org.opennms.horizon.inventory.model.Configuration;
+import org.opennms.horizon.inventory.repository.ConfigurationRepository;
+
+public class ConfigurationServiceTest {
+    private ConfigurationRepository mockConfigurationRepo;
+    private ConfigurationService service;
+
+    private ConfigurationDTO testConfiguration;
+
+    private final String location = "test location";
+    private final String systemId = "test-monitoring-system-12345";
+
+    private final String tenantId = "test-tenant";
+    private final String key = "test-key";
+    private final String value = "test-value";
+
+    @BeforeEach
+    public void setUP() {
+        mockConfigurationRepo = mock(ConfigurationRepository.class);
+        ConfigurationMapper mapper = Mappers.getMapper(ConfigurationMapper.class);
+        service = new ConfigurationService(mockConfigurationRepo, mapper);
+        testConfiguration = ConfigurationDTO.newBuilder()
+            .setLocation(location)
+            .setTenantId(tenantId)
+            .setKey(key)
+            .setValue(value)
+            .build();
+    }
+
+    @AfterEach
+    public void postTest() {
+        verifyNoMoreInteractions(mockConfigurationRepo);
+    }
+
+    @Test
+    void testCreateSingle() {
+        doReturn(Collections.singletonList(testConfiguration)).when(mockConfigurationRepo).findByTenantIdAndKey(tenantId, key);
+        service.createSingle(testConfiguration);
+        verify(mockConfigurationRepo).findByTenantIdAndKeyAndLocation(tenantId, key, location);
+        ArgumentCaptor<Configuration> configurationArgumentCaptor = ArgumentCaptor.forClass(Configuration.class);
+        verify(mockConfigurationRepo).save(configurationArgumentCaptor.capture());
+        Configuration result = configurationArgumentCaptor.getValue();
+        assertThat(result.getTenantId()).isEqualTo(tenantId);
+        assertThat(result.getLocation()).isEqualTo(location);
+        assertThat(result.getKey()).isEqualTo(key);
+        assertThat(result.getValue()).isEqualTo(value);
+    }
+}

splatch/horizon-stream

inventory/src/main/java/org/opennms/horizon/inventory/model/Configuration.java
@@ -1,0 +1,72 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2023 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2023 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.horizon.inventory.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Type;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+public class Configuration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Column(name = "tenant_id")
+    private String tenantId;
+
+    @NotNull
+    @Column(name = "key")
+    private String key;
+
+    @Column(name = "location")
+    private String location;
+
+    @NotNull
+    @Column(columnDefinition = "jsonb", name = "value")
+    @Type(type = "com.vladmihalcea.hibernate.type.json.JsonBinaryType")
+    private JsonNode value;
+}

inventory/src/test/java/org/opennms/horizon/inventory/grpc/ConfigurationGrpcTest.java
@@ -1,0 +1,116 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.horizon.inventory.grpc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.keycloak.common.VerificationException;
+import org.opennms.horizon.inventory.dto.ConfigurationDTO;
+import org.opennms.horizon.inventory.dto.ConfigurationList;
+import org.opennms.horizon.inventory.dto.ConfigurationServiceGrpc;
+import org.opennms.horizon.inventory.service.ConfigurationService;
+
+import com.google.protobuf.Empty;
+import com.google.protobuf.StringValue;
+
+import io.grpc.ManagedChannel;
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.stub.MetadataUtils;
+
+//This is an example of gRPC integration tests underline mock services.
+public class ConfigurationGrpcTest extends AbstractGrpcUnitTest {
+    private ConfigurationServiceGrpc.ConfigurationServiceBlockingStub stub;
+    private ConfigurationService mockConfigurationService;
+    private ConfigurationDTO configuration1, configuration2;
+
+    @BeforeEach
+    public void prepareTest() throws VerificationException, IOException {
+        mockConfigurationService = mock(ConfigurationService.class);
+        ConfigurationGrpcService grpcService = new ConfigurationGrpcService(mockConfigurationService, tenantLookup);
+        startServer(grpcService);
+        ManagedChannel channel = InProcessChannelBuilder.forName(serverName).directExecutor().build();
+        stub = ConfigurationServiceGrpc.newBlockingStub(channel);
+        configuration1 = ConfigurationDTO.newBuilder().setTenantId(tenantId).build();
+        configuration2 = ConfigurationDTO.newBuilder().setTenantId(tenantId).build();
+    }
+
+    @AfterEach
+    public void afterTest() {
+        verifyNoMoreInteractions(mockConfigurationService);
+        verifyNoMoreInteractions(spyInterceptor);
+        reset(mockConfigurationService, spyInterceptor);
+    }
+
+
+    @Test
+    void testListConfigurationsByTenantId() throws VerificationException {
+        doReturn(Arrays.asList(configuration1, configuration2)).when(mockConfigurationService).findByTenantId(anyString());
+        ConfigurationList result = stub.withInterceptors(MetadataUtils.newAttachHeadersInterceptor(createHeaders())).listConfigurationsByTenantId(Empty.newBuilder().build());
+        assertThat(result.getConfigurationsList().size()).isEqualTo(2);
+        verify(mockConfigurationService).findByTenantId(tenantId);
+        verify(spyInterceptor).verifyAccessToken(authHeader);
+        verify(spyInterceptor).interceptCall(any(ServerCall.class), any(Metadata.class), any(ServerCallHandler.class));
+    }
+
+    @Test
+    void testListConfigurationsByTenantIdAndLocation() throws VerificationException {
+        doReturn(Arrays.asList(configuration1, configuration2)).when(mockConfigurationService).findByTenantIdAndLocation(anyString(), anyString());
+        ConfigurationList result = stub.withInterceptors(MetadataUtils.newAttachHeadersInterceptor(createHeaders())).listConfigurationsByTenantIdAndLocation(StringValue.of("location"));
+        assertThat(result.getConfigurationsList().size()).isEqualTo(2);
+        verify(mockConfigurationService).findByTenantIdAndLocation(tenantId, "location");
+        verify(spyInterceptor).verifyAccessToken(authHeader);
+        verify(spyInterceptor).interceptCall(any(ServerCall.class), any(Metadata.class), any(ServerCallHandler.class));
+    }
+
+    @Test
+    void testListConfigurationsByTenantIdAndKey() throws VerificationException {
+        doReturn(Arrays.asList(configuration1, configuration2)).when(mockConfigurationService).findByTenantIdAndKey(anyString(), anyString());
+        ConfigurationList result = stub.withInterceptors(MetadataUtils.newAttachHeadersInterceptor(createHeaders())).listConfigurationsByTenantIdAndKey(StringValue.of("key"));
+        assertThat(result.getConfigurationsList().size()).isEqualTo(2);
+        verify(mockConfigurationService).findByTenantIdAndKey(tenantId, "key");
+        verify(spyInterceptor).verifyAccessToken(authHeader);
+        verify(spyInterceptor).interceptCall(any(ServerCall.class), any(Metadata.class), any(ServerCallHandler.class));
+    }
+}

inventory/src/test/java/org/opennms/horizon/inventory/service/ConfigurationServiceTest.java
@@ -1,0 +1,95 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.horizon.inventory.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.mockito.ArgumentCaptor;
+import org.opennms.horizon.inventory.dto.ConfigurationDTO;
+import org.opennms.horizon.inventory.mapper.ConfigurationMapper;
+import org.opennms.horizon.inventory.model.Configuration;
+import org.opennms.horizon.inventory.repository.ConfigurationRepository;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConfigurationServiceTest {
+    private ConfigurationRepository mockConfigurationRepo;
+    private ConfigurationService service;
+
+    private ConfigurationDTO testConfiguration;
+
+    private final String location = "test location";
+
+    private final String tenantId = "test-tenant";
+    private final String key = "test-key";
+    private final String value = "\"{\"test\":\"value\"}\"";
+
+    @BeforeEach
+    public void setUP() {
+        mockConfigurationRepo = mock(ConfigurationRepository.class);
+        ConfigurationMapper mapper = Mappers.getMapper(ConfigurationMapper.class);
+        service = new ConfigurationService(mockConfigurationRepo, mapper);
+        testConfiguration = ConfigurationDTO.newBuilder()
+            .setLocation(location)
+            .setTenantId(tenantId)
+            .setKey(key)
+            .setValue(value)
+            .build();
+    }
+
+    @AfterEach
+    public void postTest() {
+        verifyNoMoreInteractions(mockConfigurationRepo);
+    }
+
+    @Test
+    void testCreateSingle() throws JsonProcessingException {
+        doReturn(Collections.singletonList(testConfiguration)).when(mockConfigurationRepo).findByTenantIdAndKey(tenantId, key);
+        service.createSingle(testConfiguration);
+        verify(mockConfigurationRepo).getByTenantIdAndKeyAndLocation(tenantId, key, location);
+        ArgumentCaptor<Configuration> configurationArgumentCaptor = ArgumentCaptor.forClass(Configuration.class);
+        verify(mockConfigurationRepo).save(configurationArgumentCaptor.capture());
+        Configuration result = configurationArgumentCaptor.getValue();
+        assertThat(result.getTenantId()).isEqualTo(tenantId);
+        assertThat(result.getLocation()).isEqualTo(location);
+        assertThat(result.getKey()).isEqualTo(key);
+        assertThat(result.getValue()).isEqualTo(new ObjectMapper().readTree(value));
+    }
+}