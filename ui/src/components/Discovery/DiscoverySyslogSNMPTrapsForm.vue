b6157676711dadef06b89022301f595fe99649c9
chinh le <cle@opennms.com>
2023-02-12 04:24:30 -0500

    HS-1007 Initial

---
+++ ui/src/components/Discovery/DiscoverySyslogSNMPTrapsForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="syslog-snmp-traps-form">
+    <div class="headline">Syslog and SNMP Traps Discovery Setup</div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+@use '@featherds/styles/mixins/typography';
+
+.headline {
+  @include headline1();
+}
+</style>
