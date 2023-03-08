e5adadc482c735f5782be338ec5f4340670b44e5
Arthur Naseef <art@artnaseef.com>
2023-03-08 14:23:52 -0700

    Update the basic build-tools to work with maven changes that prevent the command-line target "jib:dockerBuild" from working on multi-module builds
    - Also improve the image push to speed up loading images into Kind
    - Remove the no-longer-used "-dev" suffix from the Grafana and Keycloak image names

--- build-tools/basic/push-images-to-kind.sh
+++ build-tools/basic/push-images-to-kind.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Please avoid committing tweaks to this script.
+
 docker pull docker.io/bitnami/kafka:3
 docker pull docker.io/bitnami/zookeeper:3.7
 docker pull mailhog/mailhog:latest
@@ -8,26 +10,49 @@ docker pull opennms/minion:29.0.10
 docker pull postgres:13.3-alpine
 docker pull busybox
 
-kind load docker-image docker.io/bitnami/kafka:3
-kind load docker-image docker.io/bitnami/zookeeper:3.7
-kind load docker-image mailhog/mailhog:latest
-kind load docker-image nginx:1.21.6-alpine
-kind load docker-image opennms/minion:29.0.10
-kind load docker-image postgres:13.3-alpine
-kind load docker-image busybox
+# Don't use "kind load" - it is slow
+# (see the bug fix for "kind load" multiple images simultaneously - when this is available, using the following kind load instead should be just as fast)
+# (bug report here: https://github.com/kubernetes-sigs/kind/issues/3063)
+# 
+# kind load docker-image docker.io/bitnami/kafka:3 \
+# 	docker.io/bitnami/zookeeper:3.7 \
+# 	mailhog/mailhog:latest \
+# 	nginx:1.21.6-alpine \
+# 	opennms/minion:29.0.10 \
+# 	postgres:13.3-alpine \
+# 	busybox \
+# 	"opennms/horizon-stream-grafana-dev:local-basic" \
+# 	"opennms/horizon-stream-core:local-basic" \
+# 	"opennms/horizon-stream-minion:local-basic" \
+# 	"opennms/horizon-stream-ui:local-basic" \
+# 	"opennms/horizon-stream-keycloak-dev:local-basic" \
+# 	"opennms/horizon-stream-inventory:local-basic" \
+# 	"opennms/horizon-stream-alarm:local-basic" \
+# 	"opennms/horizon-stream-notification:local-basic" \
+# 	"opennms/horizon-stream-rest-server:local-basic" \
+# 	"opennms/horizon-stream-minion-gateway:local-basic" \
+# 	"opennms/horizon-stream-minion-gateway-grpc-proxy:local-basic" \
+# 	"opennms/horizon-stream-metrics-processor:local-basic" \
+# 	"opennms/horizon-stream-events:local-basic" \
+# 	"opennms/horizon-stream-datachoices:local-basic"
 
-kind load docker-image "opennms/horizon-stream-grafana-dev:local-basic"
-kind load docker-image "opennms/horizon-stream-core:local-basic"
-kind load docker-image "opennms/horizon-stream-minion:local-basic"
-kind load docker-image "opennms/horizon-stream-ui:local-basic"
-kind load docker-image "opennms/horizon-stream-keycloak-dev:local-basic"
-kind load docker-image "opennms/horizon-stream-inventory:local-basic"
-kind load docker-image "opennms/horizon-stream-alert:local-basic"
-kind load docker-image "opennms/horizon-stream-notification:local-basic"
-kind load docker-image "opennms/horizon-stream-rest-server:local-basic"
-kind load docker-image "opennms/horizon-stream-minion-gateway:local-basic"
-kind load docker-image "opennms/horizon-stream-minion-gateway-grpc-proxy:local-basic"
-kind load docker-image "opennms/horizon-stream-metrics-processor:local-basic"
-kind load docker-image "opennms/horizon-stream-events:local-basic"
-kind load docker-image "opennms/horizon-stream-datachoices:local-basic"
+(
+	docker save \
+		"opennms/horizon-stream-alarm:local-basic" \
+		"opennms/horizon-stream-datachoices:local-basic" \
+		"opennms/horizon-stream-events:local-basic" \
+		"opennms/horizon-stream-grafana:local-basic" \
+		"opennms/horizon-stream-inventory:local-basic" \
+		"opennms/horizon-stream-keycloak:local-basic" \
+		"opennms/horizon-stream-metrics-processor:local-basic" \
+		"opennms/horizon-stream-minion-gateway-grpc-proxy:local-basic" \
+		"opennms/horizon-stream-minion-gateway:local-basic" \
+		"opennms/horizon-stream-minion:local-basic" \
+		"opennms/horizon-stream-notification:local-basic" \
+		"opennms/horizon-stream-rest-server:local-basic" \
+		"opennms/horizon-stream-ui:local-basic"
+) | \
+(
+	docker exec -i "kind-control-plane" ctr images import --snapshotter overlayfs -
+)
 
