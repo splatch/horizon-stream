d68beef89fb3cbc24acb81a6bf9e3384978c1f65
chinh le <cle@opennms.com>
2023-03-09 08:52:35 -0500

    HS-1170 Get updated location list in case of removal minion/node

---
+++ ui/src/store/Common/locationsQueries.ts
@@ -0,0 +1,28 @@
+import { useQuery } from 'villus'
+import { defineStore } from 'pinia'
+import { ListLocationsDocument, Location } from '@/types/graphql'
+
+export const useLocationsQueries = defineStore('locationsQueries', () => {
+  const locations = ref<Location[]>([])
+
+  const { data, execute, isFetching, error } = useQuery({
+    query: ListLocationsDocument,
+    fetchOnMount: false,
+    cachePolicy: 'network-only'
+  })
+
+  watchEffect(() => {
+    if (!error.value) {
+      if (!isFetching.value) {
+        locations.value = data.value?.findAllLocations as Location[]
+      }
+    } else {
+      // TODO: what kind of errors and how to handle them
+    }
+  })
+
+  return {
+    fetchLocations: execute,
+    locations: computed(() => locations.value || [])
+  }
+})
