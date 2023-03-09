e677edf7606a90bc8236fac3001cf6672673ba4c
chinh le <cle@opennms.com>
2023-03-09 09:17:51 -0500

    HS-1170 Remove unnecessary error handling

--- ui/src/store/Common/locationsQueries.ts
+++ ui/src/store/Common/locationsQueries.ts
@@ -5,20 +5,14 @@ import { ListLocationsDocument, Location } from '@/types/graphql'
 export const useLocationsQueries = defineStore('locationsQueries', () => {
   const locations = ref<Location[]>([])
 
-  const { data, execute, isFetching, error } = useQuery({
+  const { data, execute } = useQuery({
     query: ListLocationsDocument,
     fetchOnMount: false,
     cachePolicy: 'network-only'
   })
 
   watchEffect(() => {
-    if (!error.value) {
-      if (!isFetching.value) {
-        locations.value = data.value?.findAllLocations || []
-      }
-    } else {
-      // TODO: what kind of errors and how to handle them
-    }
+    locations.value = data.value?.findAllLocations || []
   })
 
   return {
