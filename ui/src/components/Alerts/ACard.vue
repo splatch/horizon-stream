7ae6cc2356898d68b4a965c575da21b6dae20178
chinh le <cle@opennms.com>
2023-03-11 16:44:15 -0500

    HS-1166 Add markup

--- ui/src/components/Alerts/ACard.vue
+++ ui/src/components/Alerts/ACard.vue
@@ -1,17 +1,51 @@
 <template>
   <div class="card">
-    <!-- name -->
-    <!-- severity -->
-    <!-- cause -->
-    <!-- duration -->
-    <!-- node -->
-    <!-- date/time -->
-    <!-- acknowledge ctrl -->
-    <!-- expander ctrl -->
-    <!-- description -->
+    <div class="name">{{ alert.name }}</div>
+    <div class="severity">{{ alert.severity }}</div>
+    <div class="cause">{{ alert.cause }}</div>
+    <!-- duration: hrs, days, weeks. months? -->
+    <div class="duration">{{ alert.duration }}</div>
+    <div class="node">{{ alert.node }}</div>
+    <div class="date">{{ alert.date }}</div>
+    <div class="time">{{ alert.time }}</div>
+    <div class="acknowledged">
+      <FeatherCheckbox
+        :model-value="acknowledgedChecked"
+        @update:model-value="acknowledgeHandler(alert.id)"
+        >Acknowledged</FeatherCheckbox
+      >
+    </div>
+    <div class="expander-ctrl">
+      <FeatherButton icon="Expand">
+        <FeatherIcon :icon="icon.ExpandMore" />
+      </FeatherButton>
+    </div>
+    <div class="desciption">{{ alert.description }}</div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ExpandMore from '@featherds/icon/navigation/ExpandMore'
+import ExpandLess from '@featherds/icon/navigation/ExpandLess'
+
+const props = defineProps({
+  alert: {
+    type: Object,
+    required: true
+  }
+})
+
+const acknowledgedChecked = ref(props.alert.acknowledged)
+const acknowledgeHandler = (id: number) => {
+  console.log('id', id)
+  acknowledgedChecked.value = !acknowledgedChecked.value
+  // send request
+}
+
+const icon = markRaw({
+  ExpandMore,
+  ExpandLess
+})
+</script>
 
 <style lang="scss" scoped></style>
--- ui/src/components/Alerts/ACardList.vue
+++ ui/src/components/Alerts/ACardList.vue
@@ -1,16 +1,77 @@
 <template>
   <div class="card-list">
-    <!-- select all -->
-    <!-- list count info -->
-    <!-- card list -->
-    <!-- card pagination -->
-    <div class="empty-list">
-      No results found. Refine or reduce filter criteria.
-      <FeatherButton secondary>clear all filters</FeatherButton>
+    <div class="card-list-top">
+      <div class="select-all">
+        <FeatherCheckbox v-model="selectAll" />
+        <FeatherButton
+          @click="acknowledgeAll"
+          secondary
+          >acknowledged selected</FeatherButton
+        >
+        <div class="list-count">1-50 of 100</div>
+      </div>
+    </div>
+    <div class="content">
+      <div
+        v-if="!mock.length"
+        class="empty-list"
+      >
+        No results found. Refine or reduce filter criteria.
+        <FeatherButton secondary>clear all filters</FeatherButton>
+      </div>
+      <div v-else>
+        <div
+          v-for="alert in mock"
+          :key="alert.id"
+          class="card"
+        >
+          <FeatherCheckbox
+            :model-value="isAlertSelected"
+            @update:model-value="alertSelectedHandler(alert.id)"
+          />
+          <ACard :alert="alert" />
+        </div>
+      </div>
+    </div>
+    <div class="card-list-bottom">
+      <!-- card pagination -->
+      <div class="list-count">1-50 of 100</div>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const mock = [
+  {
+    id: 1,
+    name: 'alert1',
+    severity: 'Critical',
+    cause: 'Power supply failure',
+    duration: '3hrs', //
+    node: 'Server',
+    date: '99-99-9999',
+    time: '99:99:99',
+    acknowledged: true,
+    description:
+      'Sit lorem kasd diam clita tempor ipsum justo invidunt. Elitr ut labore clita ea nonumy vero sanctus lorem. Dolores ipsum justo kasd consetetur. Dolore diam sit dolor amet clita nonumy lorem sanctus, diam voluptua kasd labore eos sadipscing, sit erat est invidunt sit stet erat et sit dolore, sit labore sadipscing nonumy diam aliquyam sanctus ea, ipsum et labore sanctus et duo sed labore amet, dolor tempor stet at sea. Stet dolores et est aliquyam. Et accusam kasd amet justo ut gubergren. Magna rebum duo sadipscing lorem kasd. Eirmod tempor dolor lorem amet at takimata consetetur voluptua. Diam labore at sanctus accusam. Accusam at lorem et amet rebum dolor at ipsum, sanctus clita lorem eirmod sit magna accusam, clita dolor no ut ea dolores no eirmod ut stet, diam sadipscing no takimata amet ipsum amet. Accusam dolor dolores takimata ut dolor sed rebum. Lorem eos consetetur ea nonumy diam nonumy gubergren invidunt consetetur. Duo labore voluptua takimata sit ipsum eirmod, consetetur vero sed at eirmod ea dolor est nonumy ipsum. Duo et accusam lorem sit dolor est dolor dolor, eirmod sea aliquyam clita et erat eos kasd est amet. Sed consetetur accusam elitr diam duo clita ut diam. Lorem magna.'
+  }
+]
+
+const selectAll = ref(false)
+watch(selectAll, (isSelected) => {
+  console.log('isSelected', isSelected)
+  isAlertSelected.value = isSelected
+})
+
+const isAlertSelected = ref(false)
+const alertSelectedHandler = (id: number) => {
+  console.log('id', id)
+  isAlertSelected.value = !isAlertSelected.value
+}
+
+const acknowledgeAll = () => {
+  // send request
+}
+</script>
 
 <style lang="scss" scoped></style>
