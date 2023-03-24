98622be9a19c2ce474d9b0b62b353d9a7b25ecbf
Anya <anya.rybalova@gmail.com>
2023-03-24 16:11:46 -0400

    HS-1236 - remove spaces

--- ui/src/components/Discovery/DiscoveryContentEditable.vue
+++ ui/src/components/Discovery/DiscoveryContentEditable.vue
@@ -159,7 +159,7 @@ const validateContent = () => {
   const regexDelim = new RegExp(props.regexDelim)
   const ceNode = document.getElementById('contentEditable_' + props.id)
   const textFromNodes = extractTextFromNodes(ceNode)
-  const contentEditableStrings = textFromNodes.split(regexDelim)
+  const contentEditableStrings = textFromNodes.replace(/\s/g, '').split(regexDelim)
   const regexp = props.regexExpression?.map((r) => new RegExp(r))
   isContentInvalid.value = false
   if (regexp) {
