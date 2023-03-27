6733670d61e2e20f6758347528829f4abbbbb16b
Rían <40221262+rcorry93@users.noreply.github.com>
2023-03-27 16:29:54 +0100

    Refactor DTO

--- ui/src/dtos/chartJS.dto.ts
+++ ui/src/dtos/chartJS.dto.ts
@@ -1,32 +1,15 @@
 import { FlowsApplicationChartData, FlowsApplicationData, FlowsLineChartItem } from '@/types'
 
 export const flowApptoChartJSLine = (data: FlowsApplicationData[]) => {
-  const appNames = [...new Set(data.map((obj) => obj.label))]
-  let objects = appNames.reduce(
-    (a, v) => ({
-      ...a,
-      [v]: {}
-    }),
-    {}
-  )
-  Object.entries(objects).forEach((app) => {
-    let objArr = []
-    objArr = data
-      .filter((item) => item.label === app[0])
-      .map((item) => ({
-        timestamp: item.timestamp,
-        value: item.value,
-        direction: item.direction
-      }))
-
-    objects = { ...objects, [app[0]]: { ...objArr } }
-  })
-
-  return Object.keys(objects).map((key: any) => {
-    const nested = objects[key as keyof typeof objects]
-    return {
-      label: key,
-      data: Object.keys(nested).map((dataKey) => ({ ...(nested[dataKey] as FlowsApplicationChartData) }))
-    } as FlowsLineChartItem
-  })
+  const transformedData = data.reduce((result: any, item) => {
+    const { label, ...data } = item
+    const existingIndex = result.findIndex((r: any) => r.label === label)
+    if (existingIndex === -1) {
+      result.push({ label, data: [data as FlowsApplicationChartData] })
+    } else {
+      result[existingIndex].data.push(data)
+    }
+    return result
+  }, [])
+  return transformedData as FlowsLineChartItem[]
 }
