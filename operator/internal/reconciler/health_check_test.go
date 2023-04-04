55acf96001dd5366506aa44c8ecd5ab5b1a7054d
Jeffrey-David Kapp <jkapp@opennms.com>
2023-04-04 09:48:24 -0400

    HS-859: operator will ping the UI as a health check on the instance

---
+++ operator/internal/reconciler/health_check_test.go
@@ -0,0 +1,83 @@
+//go:build unit
+// +build unit
+
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package reconciler
+
+import (
+	"errors"
+	"github.com/OpenNMS-Cloud/opennms-operator/internal/model/values"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"testing"
+)
+
+type mockHttp struct {
+	response *http.Response
+	err      error
+}
+
+func (h *mockHttp) Do(req *http.Request) (*http.Response, error) {
+	return h.response, h.err
+}
+
+func TestInstanceReady(t *testing.T) {
+	namespace := "mynamespace"
+	svcName := "uiServer"
+	port := 1234
+	instance := &Instance{
+		Name: "myinstance",
+		Values: values.TemplateValues{
+			Values: values.Values{
+				OpenNMS: values.OpenNMSValues{
+					UI: values.UIValues{
+						ServiceValues: values.ServiceValues{
+							ServiceName: svcName,
+							Port:        port,
+						},
+					},
+				},
+			},
+			Release: values.HelmRelease{
+				Namespace: namespace,
+			},
+		},
+	}
+	client := mockHttp{}
+	r := OpenNMSReconciler{
+		HttpClient: &client,
+		Log:        zap.New(),
+	}
+
+	client.response = &http.Response{StatusCode: http.StatusOK}
+	client.err = nil
+	res, err := r.instanceReady(instance)
+	assert.Nil(t, err, "error should be nil")
+	assert.True(t, res, "should return true when service returns 200")
+
+	client.response = &http.Response{StatusCode: http.StatusBadGateway}
+	client.err = nil
+	res, err = r.instanceReady(instance)
+	assert.Nil(t, err, "error should be nil")
+	assert.False(t, res, "should return false when service doesn't return 200")
+
+	client.response = nil
+	client.err = errors.New("this is an error")
+	res, err = r.instanceReady(instance)
+	assert.NotNil(t, err, "error shouldn't be nil")
+	assert.False(t, res, "should return false when there's an error")
+}
