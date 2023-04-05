1e4cb2f1ebd7f46fca08b8b8b09b6f30cd72d5a9
chinh le <cle@opennms.com>
2023-04-05 14:37:19 -0400

    HS-1382 Cleanup

--- ui/tests/helpers/random.ts
+++ ui/tests/helpers/random.ts
@@ -1,20 +1,14 @@
 import casual from 'casual'
 
-// export const rndBoolean = () => Math.floor(Math.random() * 100)
-
 export const rndNumber = () => Math.floor(Math.random() * 100)
 
 export const rndStatus = () => casual.random_element(['UP', 'DOWN'])
 
-// export const rndLatency = () => Math.floor(Math.random() * 150)
 export const rndLatency = () => casual.random_element(['OK', 'FAILED', 'UNKNOWN'])
 
-// export const rndUptime = () => Math.floor(Math.random() * 200)
 export const rndUptime = () => casual.random_element(['OK', 'FAILED', 'UNKNOWN'])
 
 export const rndSeverity = () => casual.random_element(['CRITICAL', 'MAJOR', 'MINOR', 'INDETERMINATE'])
-// export const rndSeverity = () => casual.random_element(['MAJOR', 'MINOR', 'WARNING', 'INDETERMINATE'])
-// export const rndSeverity = () => casual.random_element(['CRITICAL', 'MAJOR', 'MINOR', 'WARNING', 'INDETERMINATE'])
 
 export const rndDuration = () => casual.random_element(['3hrs', '1day', '1week', '1month'])
 
