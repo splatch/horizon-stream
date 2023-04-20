77695626e4e9e837f5f25fd5bffb7b2a892b5f25
Yang Li <yali@opennms.com>
2023-04-20 14:50:11 -0400

    fix the coverage error

--- inventory/main/src/test/java/org/opennms/horizon/inventory/component/NodeMonitoringManagerTest.java
+++ inventory/main/src/test/java/org/opennms/horizon/inventory/component/NodeMonitoringManagerTest.java
@@ -29,9 +29,11 @@
 package org.opennms.horizon.inventory.component;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -47,6 +49,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.opennms.horizon.events.proto.Event;
 import org.opennms.horizon.inventory.dto.NodeCreateDTO;
 import org.opennms.horizon.inventory.exception.EntityExistException;
+import org.opennms.horizon.inventory.exception.InventoryRuntimeException;
 import org.opennms.horizon.inventory.model.Node;
 import org.opennms.horizon.inventory.service.NodeService;
 import org.opennms.horizon.inventory.service.discovery.PassiveDiscoveryService;
@@ -104,5 +107,25 @@ class NodeMonitoringManagerTest {
         consumer.receiveTrapEvent(anotherEvent.toByteArray());
         verifyNoInteractions(passiveDiscoveryService);
         verifyNoInteractions(nodeService);
+    }
+
+    @Test
+    void testMissingTenantID() {
+        Event testEvent = Event.newBuilder().setUei(EventConstants.NEW_SUSPECT_INTERFACE_EVENT_UEI).build();
+        assertThatThrownBy(() ->consumer.receiveTrapEvent(testEvent.toByteArray())).isInstanceOf(InventoryRuntimeException.class);
+    }
+
+    @Test
+    void testEntityExistException() throws EntityExistException {
+        doThrow(new EntityExistException("bad request")).when(nodeService).createNode(any(NodeCreateDTO.class), eq(ScanType.NODE_SCAN), eq(tenantId));
+        ArgumentCaptor<NodeCreateDTO> argumentCaptor = ArgumentCaptor.forClass(NodeCreateDTO.class);
+        consumer.receiveTrapEvent(event.toByteArray());
+        verify(nodeService).createNode(argumentCaptor.capture(), eq(ScanType.NODE_SCAN), eq(tenantId));
+        NodeCreateDTO createDTO = argumentCaptor.getValue();
+        assertThat(createDTO.getLocation()).isEqualTo(event.getLocation());
+        assertThat(createDTO.getManagementIp()).isEqualTo(event.getIpAddress());
+        assertThat(createDTO.getLabel()).endsWith(event.getIpAddress());
+        verifyNoInteractions(passiveDiscoveryService);
+
     }
 }
