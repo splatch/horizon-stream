1fdeb2c88e2e35ae8c4ecac7154ec38f6bba1fa9
Mike <mrose@nanthealth.com>
2023-04-27 16:02:00 -0400

    Update server pckgs to fix security issues (#1072)

--- ui/mock-graphql/index.ts
+++ ui/mock-graphql/index.ts
@@ -1,9 +1,13 @@
-import { createServer } from '@graphql-yoga/node'
+import { createServer } from 'node:http'
+import { createYoga } from 'graphql-yoga'
 import schema from './src/schema'
 
 const start = async () => {
-  const server = createServer(schema)
-  await server.start()
+  const yoga = createYoga(schema)
+  const server = createServer(yoga)
+  server.listen(4000, () => {
+    console.info('GQL Mock server is running on http://localhost:4000/graphql')
+  })
 }
 
 start()
