adc017c1eb087dc5b0b37addf4cf970147a776bc
freddy61025 <fchu@opennms.com>
2023-05-15 10:41:51 -0400

    HS-1604: move flow & certificate url to helm (#1117)
    * HS-1604: move flow & certificate url to helm
    
    * HS-1604: fix test

--- metrics-processor/main/src/test/java/org/opennms/horizon/metrics/TsdataprocessorApplicationTests.java
+++ metrics-processor/main/src/test/java/org/opennms/horizon/metrics/TsdataprocessorApplicationTests.java
@@ -2,8 +2,10 @@ package org.opennms.horizon.metrics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"grpc.flow-ingestor.url=test"})
 class TsdataprocessorApplicationTests {
 
 	@Test
