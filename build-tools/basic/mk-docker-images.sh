584300420ba7a251d867bb93b6fb8c680fdebe61
DJ Gregor <dj@gregor.com>
2023-06-15 20:55:53 -0400

    mk-docker-images.sh: Allow adding command-line options to mvn invocations

--- build-tools/basic/mk-docker-images.sh
+++ build-tools/basic/mk-docker-images.sh
@@ -18,67 +18,67 @@ time {
 	echo "==="
 	echo "=== MINION IMAGE"
 	echo "==="
-	mvn -f minion/docker-assembly install -Pbuild-docker-images-enabled -DskipTests -Dapplication.docker.image=opennms/lokahi-minion:local-basic -Ddocker.skipPush=true
+	mvn -f minion/docker-assembly install -Pbuild-docker-images-enabled -DskipTests -Dapplication.docker.image=opennms/lokahi-minion:local-basic -Ddocker.skipPush=true "$@"
 
 	echo ""
 	echo "==="
 	echo "=== MINION-GATEWAY IMAGE"
 	echo "==="
-	mvn -f minion-gateway/main install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-minion-gateway:local-basic
+	mvn -f minion-gateway/main install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-minion-gateway:local-basic "$@"
 
 	echo ""
 	echo "==="
 	echo "=== MINION-CERTIFICATE-MANAGER IMAGE"
 	echo "==="
-	mvn -f minion-certificate-manager/main install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-minion-certificate-manager:local-basic
+	mvn -f minion-certificate-manager/main install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-minion-certificate-manager:local-basic "$@"
 
 	echo ""
 	echo "==="
 	echo "=== MINION-CERTIFICATE-VERIFIER IMAGE"
 	echo "==="
-	mvn -f minion-certificate-verifier/main install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-minion-certificate-verifier:local-basic
+	mvn -f minion-certificate-verifier/main install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-minion-certificate-verifier:local-basic "$@"
 
 	echo ""
 	echo "=== REST-SERVER (AKA API) IMAGE"
 	echo "==="
 	echo "==="
-	mvn -f rest-server install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-rest-server:local-basic
+	mvn -f rest-server install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-rest-server:local-basic "$@"
 
 	echo ""
 	echo "==="
 	echo "=== INVENTORY IMAGE"
 	echo "==="
-	mvn -f inventory install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-inventory:local-basic
+	mvn -f inventory install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-inventory:local-basic "$@"
 
 	echo ""
 	echo "==="
 	echo "=== ALERT SERVICE IMAGE"
 	echo "==="
-	mvn -f alert install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-alert:local-basic
+	mvn -f alert install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-alert:local-basic "$@"
 
 	echo ""
 	echo "==="
 	echo "=== NOTIFICATION IMAGE"
 	echo "==="
-	mvn -f notifications install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-notification:local-basic
+	mvn -f notifications install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-notification:local-basic "$@"
 
 	echo ""
 	echo "==="
 	echo "=== METRICS PROCESSOR IMAGE"
 	echo "==="
-	mvn -f metrics-processor install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-metrics-processor:local-basic
+	mvn -f metrics-processor install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-metrics-processor:local-basic "$@"
 
 	echo ""
 	echo "==="
 	echo "=== EVENTS IMAGE"
 	echo "==="
-	mvn -f events install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-events:local-basic
+	mvn -f events install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-events:local-basic "$@"
 
 	echo ""
 	echo "==="
 	echo "=== DATACHOICES IMAGE"
 	echo "==="
-	mvn -f datachoices install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-datachoices:local-basic
+	mvn -f datachoices install -Djib.container.creationTime=USE_CURRENT_TIMESTAMP -Dapplication.docker.image=opennms/lokahi-datachoices:local-basic "$@"
 
 	echo ""
 	echo "==="
