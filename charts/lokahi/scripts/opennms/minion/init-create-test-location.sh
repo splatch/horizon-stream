17e8a54858179c572f400363b66a13c100afacbd
DJ Gregor <dj@gregor.com>
2023-07-01 13:43:22 -0400

    Convert minion cert generator to use prepareLocationAndCerts.sh
    Add jq to the minion image and use that to run the init script.

--- charts/lokahi/scripts/opennms/minion/init-create-test-location.sh
+++ charts/lokahi/scripts/opennms/minion/init-create-test-location.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+# https://github.com/olivergondza/bash-strict-mode
+set -eEuo pipefail
 trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 chillax=10
 timeout=300
 
 deadline=$(expr $(date +%s) + $timeout)
-while [ $(date +%s) -lt $deadline ]; do
-  if ! locationId=$(bash /scripts/ensure-location-exists.sh TestLocation https://$INGRESS_HOST_PORT \
-        --connect-to $INGRESS_HOST_PORT:ingress-nginx-controller:443); then
-    echo "Well, that's not good; let's chillax for a little bit (${chillax}s) and see if things get better" >&2
-    sleep $chillax
-    continue
-  else
+while true; do
+  if [ $(date +%s) -gt $deadline ]; then
+    echo "Didn't setup location and get certificate within ${timeout}s" >&2
+    exit 1
+  fi
+
+  if bash /scripts/prepareLocationAndCerts.sh \
+              -U https://$INGRESS_HOST_PORT \
+              -k -c "--connect-to" -c "$INGRESS_HOST_PORT:ingress-nginx-controller:443" \
+              -f "/cert/minion.p12" -P changeme \
+              -l TestLocation; then
     break
   fi
-done
 
-openssl genrsa -out /cert/client.key.pkcs1 2048
-openssl pkcs8 -topk8 -in /cert/client.key.pkcs1 -out /cert/client.key -nocrypt
-openssl req -new -key /cert/client.key -out /cert/client.unsigned.cert -subj "/C=CA/ST=TBD/L=TBD/O=OpenNMS/CN=local-minion/OU=L:${locationId}/OU=T:opennms-prime"
-openssl x509 -req -in /cert/client.unsigned.cert -days 14 -CA /run/secrets/mtls/tls.crt -CAkey /run/secrets/mtls/tls.key -out /cert/client.signed.cert
-openssl pkcs12 -export -out "/cert/minion.p12" -inkey /cert/client.key -in /cert/client.signed.cert -passout "pass:changeme"
+  echo "Well, that's not good; let's chillax for a little bit (${chillax}s) and see if things get better" >&2
+  sleep $chillax
+done
